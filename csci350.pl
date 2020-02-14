f8b13289a8840452ce691e52834e8cd471d7890a
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2020-02-13 22:56:51 -0500

    Update csci350.pl

--- csci350.pl
+++ csci350.pl
@@ -1,3 +1,13 @@
 %hello world
 
-sum-up-numbers-simple(L, N).
+%sum of up the numbers in the list.
+sum([Head|Tail], N):-
+    sum(Tail, N1),
+    N is N1 + Head.
+
+%function to get only sum of numbers.
+sum-up-numbers-simple([], 0).
+
+sum-up-numbers-simple(L, N):-
+    getnumbersfromlist(L,S),
+    sum(S,N).
