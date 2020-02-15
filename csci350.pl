c679f131d1cac7eb31406ba79fea3d74c5fd4e39
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2020-02-14 22:38:18 -0500

    Update csci350.pl

--- csci350.pl
+++ csci350.pl
@@ -7,3 +7,9 @@ append([H|T],L2,[H|L3]):-append(T,L2,L3).
 
 sum-up-numbers-simple([],0).
 sum-up-numbers-simple([H|T],N):- \+(number(H))->sum-up-numbers-simple(T,N);sum-up-numbers-simple(T,N1),N is N1 + H.
+
+% flattens a nested list into a single list.
+getitems([], L) :- L=[].
+getitems([H|T], L) :-
+    getitems(T,NewListTail),
+    (not(is_list(H)) -> L = [H|NewListTail]; getitems(H,NewListHead), append(NewListHead, NewListTail, L)).
