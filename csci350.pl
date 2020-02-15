1ee905b3d8833f82c70e72fd864ef1d4477a8749
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2020-02-14 23:31:23 -0500

    Update csci350.pl

--- csci350.pl
+++ csci350.pl
@@ -3,10 +3,10 @@ append([],L,L).
 append([H|T],L2,[H|L3]):-append(T,L2,L3).
 
 % Predicate 1
-% Function to sum not nested numbers only
-
+% Function to sum a list of numbers without acknowledging nested lists
 sum-up-numbers-simple([],0).
 sum-up-numbers-simple([H|T],N):- \+(number(H))->sum-up-numbers-simple(T,N);sum-up-numbers-simple(T,N1),N is N1 + H.
+
 
 % Predicate Number 2
 % flattens a nested list into a single list.
@@ -26,6 +26,7 @@ sum-list([H|T], N):-sum-list(T, N1),N is N1 + H.
 sum-up-numbers-general([], 0).
 sum-up-numbers-general(L, N):-getitems(L,I), sum-up-numbers(I, N).
 
+
 % Predicate 4.
 %function to flatten list
 flat-list([], L):- L = [].
@@ -33,11 +34,9 @@ flat-list([], L):- L = [].
 flat-list([H|T], List) :- is_list(H), flat-list(H, List1), flat-list(T, List2), append(List1, List2, List
 flat-list([H|T], List) :- not(is_list(H))-> flat-list(T, List1), append([H], List1, List),!.
 
-
 %finds the intersection of two lists.
 intersection([], _, []). %if one list is empty, return an empty list.
 intersection(_, [], []).
-
 intersection([H|T], L, [H|NewList]) :- member(H, L), intersection(T, L, NewList),!.
 intersection([H|T], L, Trash) :- not(member(H, L)), intersection(T, L, Trash),!.
 
